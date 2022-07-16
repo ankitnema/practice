cd1a80facebc6de1913a5962bbaa801562d17c87
ankitnema <ankitnema1@gmail.com>
2022-07-16 20:27:37 +0530

    practice for the if else and if elif else condition.

---
+++ ifelse practice.py
@@ -0,0 +1,9 @@
+# This is the if else and if elif else practice for python.
+a = 10
+b = 20
+if a > b:
+    print ('a is greater than b')
+elif a == b:
+    print("a is equal to b")
+else:
+    print("b is greater than a")
