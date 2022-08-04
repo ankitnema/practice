67611f14f9ae5532e2f03e2f8c9501431930fba6
Ankit Nema <ankitnema1@gmail.com>
2022-08-04 18:21:52 +0530

    initial commit for open/read/appen/write operation

---
+++ openfile.py
@@ -0,0 +1,10 @@
+#This script to open a file and read
+x = open("file1", "a")
+#x.write(input("enter the number to add in file :  ", "\n"))
+x.write("\n")
+x.write(input("enter the a number"))
+x.close()
+
+#display added numbers in the file
+x = open("file1", "r")
+print (x.readlines()[-1])
