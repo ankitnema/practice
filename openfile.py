144cd516e080207c7eadbe0c49c7a1eb31874f74
Ankit Nema <ankitnema1@gmail.com>
2022-09-19 19:43:25 +0530

    modified file read and close method

--- openfile.py
+++ openfile.py
@@ -1,12 +1,15 @@
 #This script to open a file and read
-x = open("file1", "a")
+x = open("file1", "a+")
 #x.write(input("enter the number to add in file :  ", "\n"))
 x.write("\n")
 x.write(input("enter the a number:=   "))
 x.close()
 
-#display added numbers in the file
+#Mandatory to use "r" to display added numbers in the file
 x = open("file1", "r")
 #print (x.readlines()[-1])
+#x.close()
 print ("added last line in the file: = ", x.readlines()[-1])
-print ("transactioncompleted")
+x.close()
+print()
+print ("Transaction Completed")
