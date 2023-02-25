b07eefae57d6a7c5add6cd44229ec0ea47c0e122
Ankit Nema <ankitnema1@gmail.com>
2023-02-25 16:17:05 +0530

    practicing

--- openfile.py
+++ openfile.py
@@ -2,7 +2,7 @@
 x = open("file1", "a+")
 #x.write(input("enter the number to add in file :  ", "\n"))
 x.write("\n")
-x.write(input("enter the a number:=   "))
+x.write(input("\nenter the a number:=   "))
 x.close()
 
 #Mandatory to use "r" to display added numbers in the file
@@ -10,6 +10,8 @@ x = open("file1", "r")
 #print (x.readlines()[-1])
 #x.close()
 print ("added last line in the file: = ", x.readlines()[-1])
+#print ("\nFinal added line = ", x.readlines()[-1])
+#print (x.readlines()[-1])
 x.close()
 print()
 print ("Transaction Completed")
